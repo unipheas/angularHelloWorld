e145fd37c08455ef212e38a24a046e0ec830d863
Brian Phillips <brian.phillips@qq.com>
2017-08-05 19:04:13 +0800

    playing with services

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,19 +1,25 @@
+import { CoursesService } from './courses.service';
 import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses',
     template: `
-        <h2>{{ getTitle()  }}</h2>
+        <h2>{{ getTitle() }}</h2>
         <ul>
             <li *ngFor="let course of courses">
-                {{ course } }
+                {{ course }}
             </li>
         </ul>
     `
 })
 export class CoursesComponent {
     title = 'List of courses';
-    courses = ['course1', 'course2', 'course3'];
+
+    courses;
+
+    constructor(service: CoursesService) {
+        this.courses = service.getCourses();
+    }
 
     getTitle() {
         return this.title;
