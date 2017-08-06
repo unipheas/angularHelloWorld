b92ebf04d4d60d0707d54200d2b5f918442dac3c
Brian Phillips <brian.phillips@qq.com>
2017-08-06 12:46:53 +0800

    class binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,19 +4,9 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'courses',
     template: `
-        <button class="btn btn-primary">Save</button>
+        <button class="btn btn-primary" [class.active]="isActive">Save</button>
     `
 })
 export class CoursesComponent {
-    title = 'List of courses';
-
-    courses;
-
-    constructor(service: CoursesService) {
-        this.courses = service.getCourses();
-    }
-
-    getTitle() {
-        return this.title;
-    }
+    isActive = false;
 }
