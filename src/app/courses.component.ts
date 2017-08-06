a274a8488613b2c61a6aa47522b3a3994cabf611
Brian Phillips <brian.phillips@qq.com>
2017-08-06 12:44:47 +0800

    installed bootstrap

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,12 +4,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'courses',
     template: `
-        <h2>{{ getTitle() }}</h2>
-        <ul>
-            <li *ngFor="let course of courses">
-                {{ course }}
-            </li>
-        </ul>
+        <button class="btn btn-primary">Save</button>
     `
 })
 export class CoursesComponent {
