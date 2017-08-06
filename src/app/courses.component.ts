ed1b0d491faa7d9f11e2e4fe9abe7da19a15ad65
Brian Phillips <brian.phillips@qq.com>
2017-08-06 13:00:51 +0800

    Event Filtering

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,17 +4,11 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-courses',
     template: `
-        <div (click)="onDivClicked()">
-            <button (click)="onSave($event)">Save</button>
-        </div>
+        <input (keyup.enter)="onKeyUp()" />
     `
 })
 export class CoursesComponent {
-    onDivClicked() {
-        console.log('Div was clicked');
-    }
-    onSave($event) {
-        $event.stopPropagation();
-        console.log('Button was clicked', $event);
+    onKeyUp() {
+        console.log('Enter was pressed');
     }
 }
