54e9db3d581a1539078dca36347ab176267bd0f4
Brian Phillips <brian.phillips@qq.com>
2017-08-06 15:42:23 +0800

    better way for fav-icon

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,18 +4,19 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-courses',
     template: `
-        <span id="star" [class]="'glyphicon glyphicon-' + star" (click)="favClick()"></span>
+        <span   id="star"
+                class="glyphicon"
+                [class.glyphicon-star]="isFavorite"
+                [class.glyphicon-star-empty]="!isFavorite"
+                (click)="onClick()"
+            ></span>
     `
 })
 export class CoursesComponent {
 
-    star: any = 'star-empty';
+    isFavorite: boolean;
 
-    favClick() {
-        if (this.star === 'star') {
-            this.star = 'star-empty';
-        } else if (this.star === 'star-empty') {
-            this.star = 'star';
-        }
+    onClick() {
+        this.isFavorite = !this.isFavorite;
     }
 }
