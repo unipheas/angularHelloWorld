2d9956adea77341ca5f42886449f29f0b962e7e2
Brian Phillips <brian.phillips@qq.com>
2017-08-06 14:00:06 +0800

    dynamic fav-icon

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,11 +4,18 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-courses',
     template: `
-        {{ text | summary:30}}
+        <span id="star" [class]="'glyphicon glyphicon-' + star" (click)="favClick()"></span>
     `
 })
 export class CoursesComponent {
-    text = `
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Donec elementum ligula eu sapien consequat eleifend. Donec nec dolor erat,
-    `;
+
+    star: any = 'star-empty';
+
+    favClick() {
+        if (this.star === 'star') {
+            this.star = 'star-empty';
+        } else if (this.star === 'star-empty') {
+            this.star = 'star';
+        }
+    }
 }
