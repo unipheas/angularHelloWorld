906bbbcf4b14109a7cbfffc87602e06f731b1f78
Brian Phillips <brian.phillips@qq.com>
2017-08-07 13:09:21 +0800

    Aliasing Input Properties

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class FavoriteComponent implements OnInit {
 
-  @Input() isFavorite: boolean;
+  @Input('isFavorite') isFavorite: boolean;
 
   constructor() { }
 
