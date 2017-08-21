252c036d9efb680c83bb144403d93abeeb05d118
Brian Phillips <brian.phillips@qq.com>
2017-08-21 12:53:21 +0800

    ngFor and Change Detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,14 @@ export class AppComponent {
     { id: 1, name: 'course1'},
     { id: 2, name: 'course2'},
     { id: 3, name: 'course3'}
-  ]
+  ];
+
+  onAdd() {
+    this.courses.push({id: 4, name: 'course4'});
+  }
+
+  onRemove(course) {
+    const index = this.courses.indexOf(course);
+    this.courses.splice(index, 1);
+  }
 }
