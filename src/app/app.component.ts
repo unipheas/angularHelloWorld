b89c270f44e613a5b7f42fe41534bd396a9f9f73
Brian Phillips <brian.phillips@qq.com>
2017-08-21 13:08:26 +0800

    ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  courses;
-
-  loadCourses () {
-    this.courses = [
-      { id: 1, name: 'course1'},
-      { id: 2, name: 'course2'},
-      { id: 3, name: 'course3'}
-    ];
-  }
-
-  trackCourse(index, course) {
-    return course ? course.id : undefined;
-  }
+  
 }
