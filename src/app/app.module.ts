d64bab5a8dfd77c457944b3124213bbf4127049a
Brian Phillips <brian.phillips@qq.com>
2017-08-23 17:08:58 +0800

    setup Bootstrap form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FavoriteComponent } from './favorite/favorite.component';
 import { BootstrapPanelComponent } from './bootstrap-panel/bootstrap-panel.component';
 import { LikeComponent } from './like/like.component';
 import { InputFormatDirective } from './input-format.directive';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { InputFormatDirective } from './input-format.directive';
     FavoriteComponent,
     BootstrapPanelComponent,
     LikeComponent,
-    InputFormatDirective
+    InputFormatDirective,
+    ContactFormComponent
   ],
   imports: [
     BrowserModule,
