e79f7d00ec8ab405b55fc3aea4285a2822e2862b
Brian Phillips <brian.phillips@qq.com>
2017-08-24 22:31:02 +0800

    Working with Drop-down Lists

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactFormComponent implements OnInit {
 
+  contactMethods = [
+    {id: 1, name: 'Email'},
+    {id: 2, name: 'Phone'},
+  ];
+
   constructor() { }
 
   ngOnInit() {
