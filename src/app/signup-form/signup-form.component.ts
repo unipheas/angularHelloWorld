44af754c90bab53b97cf540a95c9f7b634600f18
Brian Phillips <brian.phillips@qq.com>
2017-08-30 14:38:17 +0800

    Implementing Custom Validation

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,3 +1,4 @@
+import { UsernameValidators } from './username.validators';
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
@@ -11,7 +12,8 @@ export class SignupFormComponent {
   form = new FormGroup({
     username: new FormControl('', [
       Validators.required,
-      Validators.minLength(3)
+      Validators.minLength(3),
+      UsernameValidators.cannotContainSpace
     ]),
     password: new FormControl('', Validators.required)
   });
