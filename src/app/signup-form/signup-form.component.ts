ae3b440dbc85908f028a4e9d6fac63922c1cfa4b
Brian Phillips <brian.phillips@qq.com>
2017-08-31 14:19:23 +0800

    Validating the Form Upon Submit && Nested FormGroups

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -10,15 +10,27 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class SignupFormComponent {
 
   form = new FormGroup({
-    username: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3),
-      UsernameValidators.cannotContainSpace
-    ], UsernameValidators.shouldBeUnique),
-    password: new FormControl('', Validators.required)
+    account: new FormGroup({
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        UsernameValidators.cannotContainSpace
+      ], UsernameValidators.shouldBeUnique),
+      password: new FormControl('', Validators.required)
+    })
   });
 
+  login () {
+    // let isValid = authService.login(this.form.value);
+    // if (!isValid) {
+    // }
+
+    this.form.setErrors({
+      invalidLogin: true
+    });
+  }
+
   get username() {
-    return this.form.get('username');
+    return this.form.get('account.username');
   }
 }
