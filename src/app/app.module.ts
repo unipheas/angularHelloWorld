ca8da5b0f8cfe60ec863d73d552f67603df474aa
Brian Phillips <brian.phillips@qq.com>
2017-09-02 21:12:56 +0800

    Seperation of Concern + Extracting a Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { PostService } from './services/post.service';
 import { HttpModule } from '@angular/http';
 import { SignupFormComponent } from './signup-form/signup-form.component';
 import { SummaryPipe } from './summary.pipe';
@@ -43,7 +44,7 @@ import { PostsComponent } from './posts/posts.component';
     ReactiveFormsModule,
     HttpModule
   ],
-  providers: [CoursesService, AuthorsService],
+  providers: [CoursesService, AuthorsService, PostService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,4 +1,4 @@
-import { Http } from '@angular/http';
+import { PostService } from './../services/post.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,14 +9,40 @@ import { Component, OnInit } from '@angular/core';
 export class PostsComponent implements OnInit {
 
   posts: any[];
-  private url = 'http://jsonplaceholder.typicode.com/posts';
 
-  constructor(private http: Http) { }
+  constructor(private service: PostService) { }
 
   ngOnInit() {
-    this.http.get(this.url)
+    this.service.getPosts()
     .subscribe(response => {
       this.posts = response.json();
     });
+  }
+
+  createPost(input: HTMLInputElement) {
+    const post = { title: input.value };
+    input.value = '';
+
+    this.service.createPosts(post)
+      .subscribe(response => {
+        post['id'] = response.json().id;
+        this.posts.splice(0, 0, post);
+      });
+  }
+
+  updatePost(post) {
+    this.service.updatePosts(post)
+      .subscribe(response => {
+        console.log(response.json());
+      });
+  }
+
+  deletePost(post) {
+    this.service.deletePosts(post.id)
+      .subscribe(response => {
+        const index = this.posts.indexOf(post);
+        this.posts.splice(index, 1);
+        console.log(response);
+      });
   }
 }
