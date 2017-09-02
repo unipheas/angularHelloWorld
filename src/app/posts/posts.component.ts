21577ad738bde7d3cf8ab2b769a9543476345435
Brian Phillips <brian.phillips@qq.com>
2017-09-02 22:36:14 +0800

    Extracting a Reusable Data Service

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -16,7 +16,7 @@ export class PostsComponent implements OnInit {
   constructor(private service: PostService) { }
 
   ngOnInit() {
-    this.service.getPosts()
+    this.service.getAll()
       .subscribe(
         response => {
           this.posts = response.json();
@@ -27,7 +27,7 @@ export class PostsComponent implements OnInit {
     const post = { title: input.value };
     input.value = '';
 
-    this.service.createPosts(post)
+    this.service.create(post)
       .subscribe(
         response => {
           post['id'] = response.json().id;
@@ -43,7 +43,7 @@ export class PostsComponent implements OnInit {
   }
 
   updatePost(post) {
-    this.service.updatePosts(post)
+    this.service.update(post)
       .subscribe(
         response => {
           console.log(response.json());
@@ -51,7 +51,7 @@ export class PostsComponent implements OnInit {
   }
 
   deletePost(post) {
-    this.service.deletePosts(post.id)
+    this.service.delete(post.id)
       .subscribe(
         response => {
           const index = this.posts.indexOf(post);
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,47 +1,11 @@
-import { BadInput } from './../common/bad-input';
-import { NotFoundError } from './../common/not-found-error';
-import { AppError } from './../common/app-error';
+import { DataService } from './data.service';
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/observable/throw';
 
 @Injectable()
-export class PostService {
-  private url = 'http://jsonplaceholder.typicode.com/posts';
+export class PostService extends DataService {
 
-  constructor(private http: Http) { }
-
-  getPosts() {
-    return this.http.get(this.url)
-      .catch(this.handleError);
-  }
-
-  createPosts(post) {
-    return this.http.post(this.url, JSON.stringify(post))
-      .catch(this.handleError);
-  }
-
-  updatePosts(post) {
-    return this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead: true}))
-      .catch(this.handleError);
-  }
-
-  deletePosts(id) {
-    return this.http.delete(this.url + '/' + id)
-      .catch(this.handleError);
-  }
-
-  private handleError(error: Response) {
-    if (error.status === 400) {
-      return Observable.throw(new BadInput(error.json()));
-    }
-
-    if (error.status === 404) {
-      return Observable.throw(new NotFoundError());
-    }
-
-    return Observable.throw(new AppError(error));
+  constructor(http: Http) {
+    super('http://jsonplaceholder.typicode.com/posts', http);
   }
 }
