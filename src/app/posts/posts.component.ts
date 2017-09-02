4faa020621994f4336a4324905d6aa611e63b90e
Brian Phillips <brian.phillips@qq.com>
2017-09-02 20:52:48 +0800

    OnInit Interface

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -11,42 +11,12 @@ export class PostsComponent implements OnInit {
   posts: any[];
   private url = 'http://jsonplaceholder.typicode.com/posts';
 
-  constructor(private http: Http) {
-    http.get(this.url)
-      .subscribe(response => {
-        this.posts = response.json();
-      });
-   }
+  constructor(private http: Http) { }
 
   ngOnInit() {
-  }
-
-  createPost(input: HTMLInputElement) {
-    const post = { title: input.value };
-    input.value = '';
-
-    this.http.post(this.url, JSON.stringify(post))
-      .subscribe(response => {
-        post['id'] = response.json().id;
-        this.posts.splice(0, 0, post);
-        // console.log(response.json());
-      });
-  }
-
-  updatePost(post) {
-    this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead: true}))
-      .subscribe(response => {
-        console.log(response.json());
-      });
-    // this.http.patch(this.url, JSON.stringify(post));
-  }
-
-  deletePost(post) {
-    this.http.delete(this.url + '/' + post.id)
-      .subscribe(response => {
-        const index = this.posts.indexOf(post);
-        this.posts.splice(index, 1);
-        console.log(response);
-      });
+    this.http.get(this.url)
+    .subscribe(response => {
+      this.posts = response.json();
+    });
   }
 }
